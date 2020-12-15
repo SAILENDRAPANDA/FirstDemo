4c7f68f3659e42dac7ffc2f0e84201dac36279e6
SAILENDRAPANDA <mosai858515@gmail.com>
2020-12-15 14:00:10 +0530

    eclipse commit

---
+++ src/CHARACTERCOUNT.java
@@ -0,0 +1,32 @@
+
+
+public class CHARACTERCOUNT {
+
+	public static void main(String[] args) {
+	String str="aamar mane titli trippin";
+	int count=0;
+	int count1=0;
+	for(int i=0;i<str.length();i++){
+		
+			if(str.charAt(i)!=' ') {
+			count++;
+	}
+	//for(int j=0;j<str.length();j++){
+	//if(str.charAt(i)==' ')
+			else 
+			{count1++;
+	}
+	}
+		System.out.println("alphabets are "+ ""+count);
+		System.out.println("spacess are "+ ""+count1);
+
+	/*str=str.replace(" ","");
+	 int count=0;
+	 for(int i=0;i<str.length();i++)
+	 {
+		 count++;
+	 }
+	 System.out.println(count);
+	}*/
+	
+}}
